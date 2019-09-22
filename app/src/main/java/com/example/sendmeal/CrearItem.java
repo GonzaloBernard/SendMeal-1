f56f6f55d660352a031dbfc675321287ab7bba7a
gonzalo <gonza.bernard.sf@gmail.com>
2019-09-22 18:27:10 -0300

    Nada imporante

--- app/src/main/java/com/example/sendmeal/CrearItem.java
+++ app/src/main/java/com/example/sendmeal/CrearItem.java
@@ -3,7 +3,6 @@ package com.example.sendmeal;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
