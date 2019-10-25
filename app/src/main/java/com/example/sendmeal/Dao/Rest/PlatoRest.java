cc237b598752ad25f12ee65dd623a1bcd7e5bfdf
guido <guido_2345@hotmail.com>
2019-10-25 12:19:12 -0300

    Creacion de clase singleton PlatoRepository, modificacion del arbol de directorios, cambie de lugar las dependencias, ahora etan bien

--- app/src/main/java/com/example/sendmeal/Dao/Rest/PlatoRest.java
+++
@@ -1,6 +0,0 @@
-package com.example.sendmeal.Dao.Rest;
-
-import com.example.sendmeal.domain.Plato;
-
-public interface PlatoRest {
-}
---
+++ app/src/main/java/com/example/sendmeal/dao/PlatorRepository.java
@@ -0,0 +1,43 @@
+package com.example.sendmeal.dao;
+
+
+import android.util.Log;
+
+import com.example.sendmeal.dao.rest.PlatoRest;
+
+
+//SINGLETON
+//https://www.youtube.com/watch?v=Z06Phxtj13g <-- tutorial
+
+public class PlatorRepository {
+
+    public static String _SERVER = "http://10.0.2.2:5000/"; //esto tengo que cambiarlo pero ni idea de como
+
+    private static PlatorRepository _INSTANCE;
+
+    private Retrofit rf;
+    private PlatoRest platoRest;
+
+    private PlatorRepository(){} //constructor privado, vacio
+
+    public static PlatorRepository getInstance(){
+
+        if(_INSTANCE == null){
+            _INSTANCE = new PlatorRepository();
+            _INSTANCE.configurarRetrofit();
+
+        }
+
+        return  _INSTANCE;
+    }
+
+    private void configurarRetrofit() {
+        new Retrofit.Builder()
+                .baseUrl(_SERVER)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Log.d("APP_2","INSTANCIA CREADA");
+        this.platoRest = this.rf.create(PlatoRest.class);
+    }
+}
+
