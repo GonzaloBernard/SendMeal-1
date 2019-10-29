b8ecc55373bb01e792510edf6202cb8ce95cca32
guido <guido_2345@hotmail.com>
2019-10-29 14:31:52 -0300

    creada las clases EstadoPedido(Enum), ItemsPedido y Pedido

---
+++ app/src/main/java/com/example/sendmeal/domain/ItemsPedido.java
@@ -0,0 +1,52 @@
+package com.example.sendmeal.domain;
+
+public class ItemsPedido {
+
+    private Integer id;
+    private Pedido pedido;
+    private Plato plato;
+    private Integer cantidad;
+    private Double precio;
+
+    public ItemsPedido(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Plato getPlato() {
+        return plato;
+    }
+
+    public void setPlato(Plato plato) {
+        this.plato = plato;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
