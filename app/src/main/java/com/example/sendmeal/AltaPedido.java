c4a5778721e42d7dbe2f84669f43c52509a9355c
gonzalo <gonza.bernard.sf@gmail.com>
2019-11-13 00:35:36 -0300

    Empezando a guardar Pedido en SQLite

--- app/src/main/java/com/example/sendmeal/AltaPedido.java
+++ app/src/main/java/com/example/sendmeal/AltaPedido.java
@@ -2,15 +2,29 @@ package com.example.sendmeal;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.example.sendmeal.dao.database.DataBaseClient;
+import com.example.sendmeal.dao.database.PedidoDao;
+import com.example.sendmeal.domain.EstadoPedido;
+import com.example.sendmeal.domain.Pedido;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class AltaPedido extends AppCompatActivity {
 
@@ -47,5 +61,45 @@ public class AltaPedido extends AppCompatActivity {
         //DECLARACION DE VIEWS
         Button buttonCreaPedido = (Button) findViewById(R.id.buttonCrearPedido);
         Button buttonEnviarPedido = (Button) findViewById(R.id.buttonEnviarPedido);
+
+        buttonCreaPedido.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date = Calendar.getInstance().getTime();
+                Pedido pedido = new Pedido();
+                //pedido.setId(01);
+                pedido.setFecha(date);
+                pedido.setEstado(EstadoPedido.PENDIENTE);
+                pedido.setLatitud(null);
+                pedido.setLongitud(null);
+
+                GuardarPedido tareaGuardarPedido = new GuardarPedido();
+                tareaGuardarPedido.execute(pedido);
+
+
+                //PedidoDao dao = DataBaseClient.getInstance(AltaPedido.this).getAppDataBase().pedidoDao();
+                //List<Pedido> listaObrasDataset =dao.getAll();
+                //Pedido p = listaObrasDataset.get(0);
+            }
+        });
     }
+    class GuardarPedido extends AsyncTask<Pedido, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Pedido... pedidos) {
+            PedidoDao dao = DataBaseClient.getInstance(AltaPedido.this).getAppDataBase().pedidoDao();
+            if(pedidos[0].getId() != null && pedidos[0].getId() >0) {
+                dao.actualizar(pedidos[0]);
+            }else {
+                dao.insert(pedidos[0]);
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
 }
+
