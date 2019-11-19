d99182066bc5cff61a596210648b9a193d42fb83
gonzalo <gonza.bernard.sf@gmail.com>
2019-11-18 23:11:25 -0300

    Manejo de mensajes push

--- app/src/main/java/com/example/sendmeal/FirebaseMessagingService.java
+++ app/src/main/java/com/example/sendmeal/FirebaseMessagingService.java
@@ -29,46 +29,13 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         // TODO(developer): Handle FCM messages here.
         Map<String,String> datas = remoteMessage.getData();
         for(Map.Entry<String,String> entrada : datas.entrySet()){
-            Log.d(TAG, "Detalle dato ( : " + entrada.getKey() +" _ "+ entrada.getValue() );
-
             //Compara si el token recibido en el mensaje push coincide con el token del usuario
             if(entrada.getKey().equals("token") && entrada.getValue().equals(getTokenFromPrefs()) ){
                 // EL MENSAJE RECIBIDO ES PARA ESTE USUARIO en particular
                 Log.d(TAG,"Mensaje personal recibido");
             }
         }
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob("dato1");
-            } else {
-                // Handle message within 10 seconds
-                handleNow("dato1");
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
-
-    private void scheduleJob(String token){
-        Log.d(TAG, "ENVIAR token: " + token);
-    }
-
-
-    private void handleNow(String token){
-        Log.d(TAG, "ENVIAR token: " + token);
-    }
-
 
     private void sendRegistrationToServer(String token){
         Log.d(TAG, "ENVIAR token: " + token);
